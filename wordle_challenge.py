c46bff2f5feda683e6c1d5e5d232b37b0c4b1515
Lucia Cerchie <luciacerchie@gmail.com>
2022-01-17 18:22:13 -0700

    first commit

---
+++ wordle_challenge.py
@@ -0,0 +1,43 @@
+# Using the rules of Wordle, given a guessWord and a solutionWord, return a set of emojis returned from the guessWord.
+
+# Example:
+
+# let solutionWord = "fudge"
+
+# $ wordleGuess("reads", solutionWord)
+# $ ""
+
+# $ wordleGuess("lodge", solutionWord)
+# $ "⬛ "
+
+emojis = {
+    "green": "💚",
+    "black": "⬛",
+    "yellow": "💛",
+}
+
+list = [] 
+
+def wordleGuess(guessWord, solutionWord):
+    "Using the rules of Wordle, given a guessWord and a solutionWord, return a set of emojis returned from the guessWord."
+    if guessWord == solutionWord:
+        length = len(guessWord)
+        print(length)
+        for i in range(length):
+            list.append(emojis['green']) 
+    if len(guessWord) != len(solutionWord):
+        print('params must be the same length')
+        
+    if guessWord != solutionWord:
+
+        for i in range(len(guessWord)):
+            if guessWord[i] not in solutionWord:
+                list.append(emojis['black'])
+            elif guessWord[i] in solutionWord and solutionWord.index(guessWord[i]) == i:
+                list.append(emojis['green'])
+            elif guessWord[i] in solutionWord:
+                list.append(emojis['yellow'])
+    print(list)
+  
+
+wordleGuess('hello', 'wrwrw2')
